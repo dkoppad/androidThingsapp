f278f52191497bae89ec9e87ce6c1bcc6c2a390e
dkoppad <dkoppad@pdx.edu>
2019-05-24 09:26:51 -0700

    added the landscape view

--- app/src/main/java/edu/pdx/rsurya07/thingsintro/MainActivity.java
+++ app/src/main/java/edu/pdx/rsurya07/thingsintro/MainActivity.java
@@ -1,9 +1,22 @@
+/**
+ * @author Dhakshayini Koppad
+ *
+ * Description:     This android things app reads and writes to a firebase realtime database.
+ *                  It writes PWM value to write to a RGB LED(pi), a 5-bit value to write to a DAC port(pi).
+ *                  It read temperature, 3 ADC values and timestamp to the database, and the motor speed.
+ *
+ *  My commits were getting pushed my public repository as Android studio is set up with dkoppad as the default.
+ *  link to my repo:
+ *              https://github.com/dkoppad/androidThingsapp.git
+ *              https://github.com/dkoppad/project3.git
+ */
+
+
 package edu.pdx.rsurya07.thingsintro;
 
 import android.os.Bundle;
 
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 
@@ -15,7 +28,6 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.sql.Timestamp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String RED_INDEX = "red";
     private static final String BLUE_INDEX = "blue";
     private static final String GREEN_INDEX = "green";
-    private static final String MOTOR_SPEED = "green";
+    private static final String DAC = "DAC";
 
     private SeekBar mRedControl;
     private SeekBar mBlueControl;
@@ -40,11 +52,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView madc2;
     private TextView madc3;
 
-    private TextView mpwm3;
     private TextView mTemp;
     private EditText mdac;
     private TextView mTimestamp;
-
+    private DatabaseReference mDatabase;
     private Handler handler = new Handler();
 
     private int pwm0 = 0;
@@ -52,14 +63,13 @@ public class MainActivity extends AppCompatActivity {
     private int pwm2 = 0;
     private int dac;
 
+    /* Variables to store values read from Firebase */
     private String pwm3_iot = String.valueOf(0);
     private String adc1_iot = String.valueOf(0);
     private String adc2_iot = String.valueOf(0);
     private String adc3_iot = String.valueOf(0);
     private String Temp_iot = String.valueOf(0);
-    private int status = 0;
-    private String mtemp =String.valueOf(0);
-    Timestamp ts;
+    private String ts;
 
 
     @Override
@@ -72,11 +82,10 @@ public class MainActivity extends AppCompatActivity {
             pwm0 = savedInstanceState.getInt(RED_INDEX, 0);
             pwm1 = savedInstanceState.getInt(GREEN_INDEX, 0);
             pwm2 = savedInstanceState.getInt(BLUE_INDEX, 0);
-            pwm3_iot = savedInstanceState.getString(MOTOR_SPEED, String.valueOf(0));
+            dac = savedInstanceState.getInt(DAC, 0);
         }
         /*
         Initial Values
-
         Set values initially to zero when the app starts up for the first time
         so that the LEDs are not lit.
          */
@@ -86,8 +95,12 @@ public class MainActivity extends AppCompatActivity {
         database.getReference().child("pwm2").setValue(pwm2);
         database.getReference().child("dac1").setValue(dac);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        /* call to get the data from Firebase */
         getDataInit();
+        mMotorSpeed = (ProgressBar) findViewById(R.id.progressBar);
+        mMSpeed = (TextView) findViewById(R.id.motorSpeed);
 
         madc1 = (TextView) findViewById(R.id.adc1);
         madc1.setText(adc1_iot);
@@ -101,13 +114,22 @@ public class MainActivity extends AppCompatActivity {
         mTemp = (TextView) findViewById(R.id.temp);
         mTemp.setText(Temp_iot);
 
+        mTimestamp = (TextView) findViewById(R.id.time);
+
         mdac = (EditText) findViewById(R.id.dac);
+
+        /* Method to see user input for the DAC Value.
+         *  the value range is between 0 to 31 so vhecking if the user input is within range and sets the
+         * value to the database.
+         * in case its more than acceptable range a tost informs user that data is out of range
+         */
         mdac.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dac = Integer.parseInt(String.valueOf(mdac));
+                dac = Integer.parseInt(mdac.getText().toString());
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                if((dac > 0) && (dac < 31)) {
+                if((dac >= 0) && (dac <= 31)) {
+                    Toast.makeText(MainActivity.this, "DAC: " + dac, Toast.LENGTH_SHORT).show();
                     database.getReference().child("dac1").setValue(dac);
 
                 }
@@ -124,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
         mRedControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
-                pwm0 = progressValue;
+                pwm0 = progressValue*255/100;
             }
 
             @Override
@@ -140,35 +162,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
         /*
-        Red Control Seek Bar
+        Green Control Seek Bar
          */
         mGreenControl = (SeekBar) findViewById(R.id.greenbar);
         mGreenControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
-                pwm1 = progressValue;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(MainActivity.this, "Intensity:" + pwm1, Toast.LENGTH_SHORT).show();
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                database.getReference().child("pwm1").setValue(pwm1);
-            }
-        });
-
-        /*
-        Blue Control Seek Bar
-         */
-        mBlueControl = (SeekBar) findViewById(R.id.bluebar);
-        mBlueControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
-                pwm2 = progressValue;
+                pwm2 = progressValue*255/100;
             }
 
             @Override
@@ -183,35 +183,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         /*
-        Motor Speed Progress Bar
+        Blue Control Seek Bar
          */
-        mMotorSpeed = (ProgressBar) findViewById(R.id.progressBar);
-        mMSpeed = (TextView) findViewById(R.id.motorSpeed);
-        status = Integer.parseInt(pwm3_iot);
-        new Thread(new Runnable() {
+        mBlueControl = (SeekBar) findViewById(R.id.bluebar);
+        mBlueControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void run() {
-                while (status < 100) {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            mMotorSpeed.setProgress(status);
-                            mMSpeed.setText(mMSpeed + "/ 100");
-
-                        }
-                    });
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                pwm1 = progressValue*255/100;
             }
-        }).start();
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
 
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Intensity:" + pwm1, Toast.LENGTH_SHORT).show();
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                database.getReference().child("pwm1").setValue(pwm1);
+            }
+        });
     }
 
 
@@ -226,28 +218,46 @@ public class MainActivity extends AppCompatActivity {
         savedInstanceState.putInt(RED_INDEX, pwm0);
         savedInstanceState.putInt(GREEN_INDEX, pwm1);
         savedInstanceState.putInt(BLUE_INDEX, pwm2);
+        savedInstanceState.putInt(DAC, dac);
     }
 
+    /* getDataInit() function acess Firebase and reads the ADC, Motor Speed, timestamp and Temp to
+     * to display on the app*
+     */
+
     private void getDataInit() {
-        ValueEventListener dataListner = new ValueEventListener() {
+        ValueEventListener dataListener = new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onDataChange(DataSnapshot dataSnapshot) {
                 //Get data from database
                 pwm3_iot = dataSnapshot.child("pwm3").getValue().toString();
                 adc1_iot = dataSnapshot.child("adc3").getValue().toString();
                 adc2_iot = dataSnapshot.child("adc4").getValue().toString();
                 adc3_iot = dataSnapshot.child("adc5").getValue().toString();
                 Temp_iot = dataSnapshot.child("Temp").getValue().toString();
-                //ts = dataSnapshot.child("Timestamp").getValue().toString();
+                ts = dataSnapshot.child("Timestamp").getValue().toString();
 
+                mTimestamp.setText("Last response from connect device: \n" + ts);
+                mTemp.setText("Temperature: " + Temp_iot + "C");
+
+                madc1.setText("ADC3\n  "+adc1_iot);
+                madc2.setText("ADC4\n  "+adc2_iot);
+                madc3.setText("ADC5\n  "+adc3_iot);
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mMotorSpeed.setProgress(Integer.parseInt(pwm3_iot)*100/255);
+                        mMSpeed.setText(Integer.parseInt(pwm3_iot)*100/255 + "%");
+                    }
+                });
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         };
+        mDatabase.addValueEventListener(dataListener);
     }
-
-
 }
